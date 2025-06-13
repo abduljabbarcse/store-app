00cbc03197c7f2e25b3adb0724c9df0191675b98
abduljabbarcse <abduljabbarcse2001@gmail.com>
2025-06-13 22:48:59 +0530

    fix

--- components/Main/ProductFilters.tsx
+++ components/Main/ProductFilters.tsx
@@ -10,6 +10,7 @@ import {
     setOpenFilter
 } from "@/lib/slices/productCatalogSlice";
 import { filterGroupOptions } from "@/utils/utils";
+import { FilterGroups } from "@/type/interFaces";
 
 export default function ProductFilters() {
     const { openFilter, filterOptions } = useAppSelector((state) => state.productCatalog);
@@ -29,71 +30,78 @@ export default function ProductFilters() {
                 </label>
             </div>
             <div className="filter-divider" />
-            {Object.keys(filterGroupOptions).map((group) => (
-                <div className="filter-group" key={group}>
-                    <div
-                        className="filter-group-title-row"
-                        onClick={() =>
-                            dispatch(setOpenFilter(openFilter === group ? null : group))
-                        }
-                        style={{ cursor: "pointer" }}
-                    >
-                        <span className="filter-title font-sora">{group}</span>
+            {Object.keys(filterGroupOptions).map((group) => {
+                const typedGroup = group as keyof FilterGroups;
 
-                        <span
-                            style={{
-                                cursor: "pointer",
-                                display: "inline-flex",
-                                marginLeft: "0.2rem",
-                                transition: "transform 0.2s ease",
-                                transform: openFilter === group ? "rotate(180deg)" : "none",
-                            }}
-                        >
-                            <ChevronDown size={20} />
-                        </span>
-                    </div>
-                    <div className="filter-group-dropdown">
-                        <span
-                            className="filter-group-all font-sora"
-                            onClick={() => dispatch(handleSelectAll(group))}
+                return (
+                    <div className="filter-group" key={group}>
+                        <div
+                            className="filter-group-title-row"
+                            onClick={() =>
+                                dispatch(setOpenFilter(openFilter === typedGroup ? null : typedGroup))
+                            }
                             style={{ cursor: "pointer" }}
                         >
-                            All
-                        </span>
-                    </div>
-                    {openFilter === group && (
-                        <div className="filter-options">
-                            <a
-                                href="#"
-                                className="filter-unselect-all font-sora"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    dispatch(handleUnselectAll(group));
+                            <span className="filter-title font-sora">{group}</span>
+
+                            <span
+                                style={{
+                                    cursor: "pointer",
+                                    display: "inline-flex",
+                                    marginLeft: "0.2rem",
+                                    transition: "transform 0.2s ease",
+                                    transform: openFilter === typedGroup ? "rotate(180deg)" : "none",
                                 }}
                             >
-                                Unselect all
-                            </a>
-                            {filterGroupOptions[group].map((option) => (
-                                <label className="filter-option font-sora" key={option}>
-                                    <input
-                                        type="checkbox"
-                                        checked={filterOptions[group].includes(option)}
-                                        onChange={(e) =>
-                                            dispatch(handleFilterCheck({
-                                                group,
-                                                option,
-                                                checked: e.target.checked
-                                            }))
-                                        }
-                                    />
-                                    {option}
-                                </label>
-                            ))}
+                                <ChevronDown size={20} />
+                            </span>
                         </div>
-                    )}
-                    <div className="filter-divider" />
-                </div>
-            ))}
+
+                        <div className="filter-group-dropdown">
+                            <span
+                                className="filter-group-all font-sora"
+                                onClick={() => dispatch(handleSelectAll(typedGroup))}
+                                style={{ cursor: "pointer" }}
+                            >
+                                All
+                            </span>
+                        </div>
+
+                        {openFilter === typedGroup && (
+                            <div className="filter-options">
+                                <a
+                                    href="#"
+                                    className="filter-unselect-all font-sora"
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        dispatch(handleUnselectAll(typedGroup));
+                                    }}
+                                >
+                                    Unselect all
+                                </a>
+                                {filterGroupOptions[typedGroup].map((option) => (
+                                    <label className="filter-option font-sora" key={option}>
+                                        <input
+                                            type="checkbox"
+                                            checked={filterOptions[typedGroup].includes(option)}
+                                            onChange={(e) =>
+                                                dispatch(handleFilterCheck({
+                                                    group: typedGroup,
+                                                    option,
+                                                    checked: e.target.checked
+                                                }))
+                                            }
+                                        />
+                                        {option}
+                                    </label>
+                                ))}
+                            </div>
+                        )}
+                        <div className="filter-divider" />
+                    </div>
+                );
+            })}
+
         </aside>
     );
 }
